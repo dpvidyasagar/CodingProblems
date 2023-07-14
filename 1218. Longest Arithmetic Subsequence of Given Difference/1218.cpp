15fca5348f44c0bb9dcb65f1c5f0a561dc969b16
code4mn <vidya.17255@knit.ac.in>
2023-07-15 00:32:12 +0530

    Leetcode 1218

--- 1218. Longest Arithmetic Subsequence of Given Difference/1218.cpp
+++ 1218. Longest Arithmetic Subsequence of Given Difference/1218.cpp
@@ -1,3 +1,21 @@
+class Solution {
+private:
+    int find(int index,int last,int d,int n,vector<int>& nums,vector<vector<int>>&dp){
+        if(index >= n) return 0;
+        int take = 0;
+        int notTake = 0;
+        // if(dp[index][last] != -1) return dp[index][last];
+        if(last == 1e9){
+            notTake = find(index+1,last,d,n,nums,dp);
+            take = 1+ find(index+1,nums[index],d,n,nums,dp);
+        }else{
+            notTake = find(index+1,last,d,n,nums,dp);
+            if(nums[index] - last == d){
+                take = 1+ find(index+1,nums[index],d,n,nums,dp);
+            }
+        }
+        return max(take,notTake);
+    }
 public:
     int longestSubsequence(vector<int>& arr, int d) {
         int n = arr.size();
